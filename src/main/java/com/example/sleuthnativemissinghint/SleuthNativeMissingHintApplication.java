e39ceea44336ac504c54ccebe3a70a74ef4bd7b9
Damian Jorda <damian.jjorda@gmail.com>
2022-07-06 09:11:55 -0300

    Initial commit

---
+++ src/main/java/com/example/sleuthnativemissinghint/SleuthNativeMissingHintApplication.java
@@ -0,0 +1,13 @@
+package com.example.sleuthnativemissinghint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthNativeMissingHintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthNativeMissingHintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sleuthnativemissinghint/SleuthNativeMissingHintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sleuthnativemissinghint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthNativeMissingHintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
